a796811f29faa4e120de23980e2009fccb59228a
jorgeaveraf <jorgeaveraf@gmail.com>
2024-11-29 23:41:45 -0600

    Changes for be able to print styles

--- modulo_secundaria/settings.py
+++ modulo_secundaria/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 import firebase_admin
 from firebase_admin import credentials, auth
@@ -133,6 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Donde se recopilan los archivos después de collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 GRAPHENE = {
     'SCHEMA': 'modulo_secundaria.schema.schema',
--- modulo_secundaria/urls.py
+++ modulo_secundaria/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf.urls.static import static
+
+from modulo_secundaria import settings
 
 
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
